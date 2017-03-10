07ed9e9268c7db75940dbda34f906c99ef5c4c4f
seanLetmon97 <sean.letmon@ucdconnect.ie>
2017-03-10 17:24:09 +0000

    Header file containing definitions of slot functions

---
+++ slots.h
@@ -0,0 +1,17 @@
+/*
+ * slots.h
+ *
+ *  Created on: 7 Mar 2017
+ *      Author: Admin
+ */
+
+#ifndef SLOTS_H_
+#define SLOTS_H_
+
+#include"structdefs.h"
+
+void slotCreation(struct slots array[],struct players record[], int k);
+
+void SlotPrint(struct slots array[],struct players record[],int k);
+
+#endif /* SLOTS_H_ */
