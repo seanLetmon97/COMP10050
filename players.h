3eeaa27cea67a79e565da0e96f962ee71e5577bc
Bill-IN <will2@outlook.ie>
2017-03-10 13:22:25 +0000

    Header file containing definitions for players functions

---
+++ players.h
@@ -0,0 +1,18 @@
+/*
+ * players.h
+ *
+ *  Created on: 7 Mar 2017
+ *      Author: SK
+ */
+
+#ifndef PLAYERS_H_
+#define PLAYERS_H_
+
+#include"structdefs.h"
+
+void PlayerCreation(struct players record[],int k);
+
+void playerPrint(struct players record[],int k);
+
+
+#endif /* PLAYERS_H_ */
